cc4871df6790968c25cc1970910af1e5c9182e9a
Sai Vasanth <vasanthboggarapu@gmail.com>
2026-06-19 16:35:39 +0530

    Fix CORS issue

--- server/server.js
+++ server/server.js
@@ -12,24 +12,36 @@ connectDB();
 
 const app = express();
 
-const cors = require("cors");
-
+// CORS FIX
 app.use(
   cors({
     origin: true,
     credentials: true,
   })
 );
-app.use(express.json());
-app.use("/api/auth", authRoutes);
-app.use("/api/trips",tripRoutes);
 
+app.use(express.json());
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/trips", tripRoutes);
+
+// Test Route
 app.get("/", (req, res) => {
   res.send("API Running");
+});
+
+// Debug Route
+app.get("/test", (req, res) => {
+  res.json({
+    message: "Backend Updated",
+  });
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(
+    `Server running on port ${PORT}`
+  );
 });
