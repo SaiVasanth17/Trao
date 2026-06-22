431fffd876c5108d5e25d1b850c51695fc8dee8c
Sai Vasanth <vasanthboggarapu@gmail.com>
2026-06-22 14:01:31 +0530

    Fix server

--- server/server.js
+++ server/server.js
@@ -12,26 +12,17 @@ connectDB();
 
 const app = express();
 
-// CORS FIX
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json());
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/trips", tripRoutes);
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("API Running");
 });
 
-// Debug Route
 app.get("/test", (req, res) => {
   res.json({
     message: "Backend Updated",
@@ -41,7 +32,5 @@ app.get("/test", (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
+  console.log(`Server running on port ${PORT}`);
 });
